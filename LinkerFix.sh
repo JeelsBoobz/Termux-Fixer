986e0d4fb78f0c0818cd56bdbb6177b3ceb0e48d
JeelsBoobz <38210868+JeelsBoobz@users.noreply.github.com>
2018-12-14 17:34:13 +0700

    Update LinkerFix.sh

--- LinkerFix.sh
+++ LinkerFix.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 apt install git make clang
+cd $HOME
 git clone https://github.com/Harshiv-Patel/termux-elf-cleaner.git
 cd termux-elf-cleaner
 make
@@ -8,4 +9,5 @@ cp ./termux-elf-cleaner ../..
 cp ./termux-elf-cleaner $PREFIX/bin/
 termux-elf-cleaner /data/data/com.termux/files/usr/lib/libcrypto.so
 termux-elf-cleaner /data/data/com.termux/files/usr/lib/libssl.so
-rm -Rf termux-elf-cleaner
+cd $HOME
+rm -Rf $HOME/termux-elf-cleaner

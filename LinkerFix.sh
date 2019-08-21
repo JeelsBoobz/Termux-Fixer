d7715a4647be2bc24411b2fcb402e869de795113
JeelsBoobz <38210868+JeelsBoobz@users.noreply.github.com>
2019-08-21 09:18:05 +0700

    Update LinkerFix.sh

--- LinkerFix.sh
+++ LinkerFix.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 cd $HOME
-if [ -z $PREFIX/bin/termux-elf-cleaner ]; then
+if [ -z $PREFIX/bin/termux-elf-cleaner ]
+then
 	apt install git make clang
 	git clone https://github.com/Harshiv-Patel/termux-elf-cleaner.git
 	cd termux-elf-cleaner
@@ -8,7 +9,7 @@ if [ -z $PREFIX/bin/termux-elf-cleaner ]; then
 	cp ./termux-elf-cleaner ../..
 	../../termux-elf-cleaner ./termux-elf-cleaner
 	cp ./termux-elf-cleaner $PREFIX/bin/
+	rm -Rf $HOME/termux-elf-cleaner
+	cd $HOME
 fi
 for i in $PREFIX/lib/*.so* do [ -z "$(readelf -d "$i" |grep NODELETE)" ] || termux-elf-cleaner "$i"; done 
-cd $HOME
-rm -Rf $HOME/termux-elf-cleaner

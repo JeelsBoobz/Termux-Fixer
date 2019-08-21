5695b42652aeb9ffc39247790be4dc06b9251996
JeelsBoobz <38210868+JeelsBoobz@users.noreply.github.com>
2019-08-21 09:07:38 +0700

    Update LinkerFix.sh

--- LinkerFix.sh
+++ LinkerFix.sh
@@ -7,7 +7,6 @@ make
 cp ./termux-elf-cleaner ../..
 ../../termux-elf-cleaner ./termux-elf-cleaner
 cp ./termux-elf-cleaner $PREFIX/bin/
-termux-elf-cleaner /data/data/com.termux/files/usr/lib/libcrypto.so
-termux-elf-cleaner /data/data/com.termux/files/usr/lib/libssl.so
+for i in $PREFIX/lib/*.so* do [ -z "$(readelf -d "$i" |grep NODELETE)" ] || termux-elf-cleaner "$i"; done 
 cd $HOME
 rm -Rf $HOME/termux-elf-cleaner
